3e11ab15981c83d330ed0466f5f6ca4905cf0b17
alexbeattie42 <alexander.beattie@quarkworks.co>
2022-04-28 10:51:26 +0300

    Update shortcuts

--- home.nix
+++ home.nix
@@ -81,8 +81,6 @@
       setlocal spell spelllang=en_us "local region
       set spelllang=en_us "spell check
       set spellsuggest=best,9 "9 Best Choices
-      nnoremap <silent> <F11> :set spell!<cr> "Remap f11 so toggle spell check
-      inoremap <silent> <F11> <C-O>:set spell!<cr>
     '';
 
     # Neovim plugins
